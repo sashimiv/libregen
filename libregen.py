c7b4305b8e06a635f83b3e1ebd2d81c7cc43c548
sashimiv <74048469+sashimiv@users.noreply.github.com>
2022-11-25 16:07:09 +0300

    Create libregen.py

---
+++ libregen.py
@@ -0,0 +1,14 @@
+import random
+
+chars = '+-/*!&$#?=@<>[\]•^_abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = input('number of passwords?'+ "\n")
+length = input('password length?'+ "\n")
+number = int(number)
+for n in range(number):
+    password =''
+    for i in range(length)
+        password = random.choice(chars)
+    print(password) 
+    print("copyright 2022 sashimiv")
+    print("github.com/sashimiv/libregen")
+    print("telegram: @sashimiv")
