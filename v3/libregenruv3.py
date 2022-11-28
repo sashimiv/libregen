6df2f2a8286e57c1a68cdff44debb2bb8cb3aec3
sashimiv <74048469+sashimiv@users.noreply.github.com>
2022-11-28 20:16:07 +0300

    Create libregenruv3.py
    аче)

---
+++ v3/libregenruv3.py
@@ -0,0 +1,23 @@
+import random # импорт библеотеки random
+chars = '+-/*!&$#?=@<>[\]{^_a}bcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' # символы для генерации
+
+q = random.choice(chars) # создает символ под переменной q
+w = random.choice(chars) # создает символ под переменной w
+e = random.choice(chars) # создает символ под переменной e
+r = random.choice(chars) # создает символ под переменной r
+t = random.choice(chars) # создает символ под переменной t
+y = random.choice(chars) # создает символ под переменной y
+u = random.choice(chars) # создает символ под переменной u
+o = random.choice(chars) # создает символ под переменной o
+p = random.choice(chars) # создает символ под переменной p
+a = random.choice(chars) # создает символ под переменной a
+s = random.choice(chars) # создает символ под переменной s
+d = random.choice(chars) # создает символ под переменной d
+print("https://github.com/sashimiv/libregen") # github проекта
+print(" ") # разделитель?
+zxc = q+w+e+r+t+y+u+o+p+a+s+d # соединяет все символы в 1 переменную
+print(zxc) # печатает переменную
+# версия 3
+# просто использует библеотеку random
+# by sashimiv 2022
+# Telegram @sashimivl
