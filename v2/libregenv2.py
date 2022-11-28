a909a7f9edb6b0f0f003949a9068ded6547c38f3
sashimiv <74048469+sashimiv@users.noreply.github.com>
2022-11-28 20:15:06 +0300

    Create libregenv2.py
    неудачно но работает

---
+++ v2/libregenv2.py
@@ -0,0 +1,33 @@
+import random
+chars = '+-/*!&$#?=@<>[\]{^_a}bcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+length = int(1)
+for i in range(length):
+   q = random.choice(chars)
+for i in range(length):
+   w = random.choice(chars)
+for i in range(length):
+   e = random.choice(chars)
+for i in range(length):
+   r = random.choice(chars)
+for i in range(length):
+   t = random.choice(chars)
+for i in range(length):
+   y = random.choice(chars)
+for i in range(length):
+   u = random.choice(chars)
+for i in range(length):
+   o = random.choice(chars)
+for i in range(length):
+   p = random.choice(chars)
+for i in range(length):
+   a = random.choice(chars)
+for i in range(length):
+   s = random.choice(chars)
+for i in range(length):
+   d = random.choice(chars)
+
+zxc = q+w+e+r+t+y+u+o+p+a+s+d
+
+print(zxc)
+#Telegram @sashimiv
+#https://github.com/sashimiv/libregen/
