dcf73a81c7f916f484bce2b20d597b35cb190b74
sashimiv <74048469+sashimiv@users.noreply.github.com>
2022-11-29 20:27:17 +0300

    Add files via upload

---
+++ v3/librepassv3ch.py
@@ -0,0 +1,25 @@
+# версия с выбором символов для генерации
+import random # импорт библеотеки random
+chars = input("введите символы (будут использоваться для генерации)") # символы для генерации
+
+q = random.choice(chars) # создает символ под переменной q
+w = random.choice(chars) # создает символ под переменной w
+e = random.choice(chars) # создает символ под переменной e
+r = random.choice(chars) # создает символ под переменной r
+t = random.choice(chars) # создает символ под переменной t
+y = random.choice(chars) # создает символ под переменной y
+u = random.choice(chars) # создает символ под переменной u
+o = random.choice(chars) # создает символ под переменной o
+p = random.choice(chars) # создает символ под переменной p
+a = random.choice(chars) # создает символ под переменной a
+s = random.choice(chars) # создает символ под переменной s
+d = random.choice(chars) # создает символ под переменной d
+print("https://github.com/sashimiv/libregen") # github проекта
+print(" ") # разделитель?
+zxc = q+w+e+r+t+y+u+o+p+a+s+d # соединяет все символы в 1 переменную
+print(zxc) # печатает переменную
+# версия 3
+# просто использует библеотеку random
+# by sashimiv 2022
+# Telegram @sashimivl
+
